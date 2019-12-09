09c8167e1fe6f0ecb40cd43f287095a46622c90c
Eric LaForce <elaforc@gmail.com>
2019-12-09 08:53:33 -0500

    Updated to run on OSX

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "node MyBot.js" "node MyBot.js"
+./halite_osx --replay-directory replays/ -vvv --width 32 --height 32 --results-as-json "node NifflerBot.js --halitemax=4549 --maxships=2 --capacity=969 --recreate=102 --fitnessformaxships=2991 --fitnessfordistancetodropoff=3 --halitecellmodifier=36 --modifierturnsships=5"
